fc52c5fab5452abaab25f9014df5da9de9c2b25d
hhavryliuk <hhavryliuk@microstrategy.com>
2021-04-10 22:46:01 +0200

    add eslint

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Here we go!')
+  res.send('Here we go!');
 });
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
