b80c384fa812fb3f34bc8facf41a8b62c09bb3b4
hhavryliuk <hhavryliuk@microstrategy.com>
2021-04-14 10:11:03 +0200

    start:dev with ts-node-dev + some refactor

--- index.ts
+++ index.ts
@@ -1,11 +1,20 @@
 import express from 'express';
-import db from './src/db';
+import mongoose from 'mongoose';
+import { dbURI } from './src/config/db-config';
 
 const app = express();
 const port = 3000;
 
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
 app.get('/', (req, res) => {
-  res.send('Here we go!');
+  res.send('Here we goooo!');
 });
 
 app.listen(port, () => {
